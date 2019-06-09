ec97a72de90652f6ff33430dab0b15e7f1daa8af
Ismail Alaoui Abdellaoui <60974@aui.ma>
2019-06-09 16:50:59 +0200

    update

--- code/part3_3d.py
+++ code/part3_3d.py
@@ -51,25 +51,34 @@ def triangulation_plotting_library(matches_library,camera1_library,camera2_libra
         
         #minimal algebraic error- P determined by SVD
         uc1l, sc1l, vc1l = np.linalg.svd(test)
-        #uc2l, sc2l, vc2l = np.linalg.svd(camera_2_library)
-        #finding the values of F
-        fc1l = vc1l[np.argmin(sc1l)]
-        #calculating the SVD of F
-        ufc1l, sfc1l, vhfc1l = np.linalg.svd(fc1l.reshape(2,2).T)
+#
+#        #finding the values of F
+#        fc1l = vc1l[np.argmin(sc1l)]
+#        #calculating the SVD of F
+#        ufc1l, sfc1l, vhfc1l = np.linalg.svd(fc1l.reshape(2,2).T)
+#        
+#        #setting the smallest singular favue to 0
+#        sfc1l[np.argmin(sfc1l)] = 0
+#        
+#        #recalculate F values
+#        smat = np.diag(sfc1l)
+#        quasi_f = np.linalg.multi_dot([ufc1l, smat , vhfc1l])
         
-        #setting the smallest singular favue to 0
-        sfc1l[np.argmin(sfc1l)] = 0
+        temp = (-1) * vc1l[-1]  
+        temp = temp / temp[-1]  
         
-        #recalculate F values
-        smat = np.diag(sfc1l)
-        quasi_f = np.linalg.multi_dot([ufc1l, smat , vhfc1l])
-        points_3d_library[i] = quasi_f.T.reshape(1,4)
+        
+        
+        points_3d_library[i] = temp.T.reshape(1,4)
         
     camera_center1 = compute_camera_center(camera_1_library)
+    camera_center1 = camera_center1/camera_center1[-1]
+    
     camera_center2 = compute_camera_center(camera_2_library)
+    camera_center2 = camera_center2/camera_center2[-1]
     
     fig = plt.figure()
-    fig.suptitle('3D reconstruction of the library', fontsize=16)
+    fig.suptitle('3D reconstruction of the Library', fontsize=16)
     ax = fig.add_subplot(111, projection="3d")
     ax.scatter(camera_center1[0],camera_center1[1],camera_center1[2],c='r',marker='+',label="Camera 1 center")
     ax.scatter(camera_center2[0],camera_center2[1],camera_center2[2],c='g',marker='+',label="Camera 2 center")
@@ -80,6 +89,7 @@ def triangulation_plotting_library(matches_library,camera1_library,camera2_libra
     plt.xlabel("x axis")
     plt.ylabel("y axis")
 #    plt.zlabel("z axis")
+    return points_3d_library
 
 
 
@@ -119,44 +129,29 @@ def triangulation_plotting_house(matches_house,camera_1_house,camera_2_house):
         uc1l, sc1l, vc1l = np.linalg.svd(test)
         #uc2l, sc2l, vc2l = np.linalg.svd(camera_2_library)
         #finding the values of F
-        fc1l = vc1l[np.argmin(sc1l)]
-        #calculating the SVD of F
-        ufc1l, sfc1l, vhfc1l = np.linalg.svd(fc1l.reshape(2,2).T)
+#        fc1l = vc1l[np.argmin(sc1l)]
+#        #calculating the SVD of F
+#        ufc1l, sfc1l, vhfc1l = np.linalg.svd(fc1l.reshape(2,2).T)
+#        
+#        #setting the smallest singular favue to 0
+#        sfc1l[np.argmin(sfc1l)] = 0
+#        
+#        #recalculate F values
+#        smat = np.diag(sfc1l)
+#        quasi_f = np.linalg.multi_dot([ufc1l, smat , vhfc1l])
         
-        #setting the smallest singular favue to 0
-        sfc1l[np.argmin(sfc1l)] = 0
+        temp = (-1) * vc1l[-1]  
+        temp = temp / temp[-1]  
+#        
         
-        #recalculate F values
-        smat = np.diag(sfc1l)
-        quasi_f = np.linalg.multi_dot([ufc1l, smat , vhfc1l])
-        points_3d_house[i] = quasi_f.T.reshape(1,4)
+        
+        points_3d_house[i] = temp.T.reshape(1,4)
         
     camera_center1 = compute_camera_center(camera_1_house)
-    camera_center2 = compute_camera_center(camera_2_house)
+    camera_center1 = camera_center1/camera_center1[-1]
     
-    fig = plt.figure()
-    fig.suptitle('3D reconstruction of the house', fontsize=16)
-    ax = fig.add_subplot(111, projection="3d")
-    ax.scatter(camera_center1[0],camera_center1[1],camera_center1[2],c='r',marker='+',label="Camera 1 center")
-    ax.scatter(camera_center2[0],camera_center2[1],camera_center2[2],c='g',marker='+',label="Camera 2 center")
-    for i in range(len(points_3d_house)):
-        ax.scatter(points_3d_house[i][0],points_3d_house[i][1],points_3d_house[i][2],c='b')
-    plt.legend()
-    plt.axis('equal')
-    plt.xlabel("x axis")
-    plt.ylabel("y axis")
-#    plt.zlabel("z axis")
-    
-
-triangulation_plotting_library(matches_library,camera_1_library,camera_2_library)
-triangulation_plotting_house(matches_house,camera_1_house,camera_2_house)
-def test_house_triangulation(matches_house,camera_1_house,camera_2_house):
-    points_3d_house = np.zeros((len(matches_house),4),dtype=float)
-    for i in range(len(matches_house)):
-#        print(matches_house[i,2:4].shape)
-        points_3d_house[i] = cv2.triangulatePoints(camera_1_house, camera_2_house, matches_house[i,0:2], matches_house[i,2:4]).reshape(1,4)
-    camera_center1 = compute_camera_center(camera_1_house)
     camera_center2 = compute_camera_center(camera_2_house)
+    camera_center2 = camera_center2/camera_center2[-1]
     
     fig = plt.figure()
     fig.suptitle('3D reconstruction of the house', fontsize=16)
@@ -172,8 +167,74 @@ def test_house_triangulation(matches_house,camera_1_house,camera_2_house):
 #    plt.zlabel("z axis")
     return points_3d_house
 
-#def test_library_triangulation(matches_)
-#test_house_triangulation(matches_house,camera_1_house,camera_2_house)
+
+
+
+
+
+
+
+
+def test_library_triangulation(matches_library,camera_1_library,camera_2_library):
+    points_3d_library = np.zeros((len(matches_library),4),dtype=float)
+    for i in range(len(matches_library)):
+#        print(matches_house[i,2:4].shape)
+        points_3d_library[i] = cv2.triangulatePoints(camera_1_library, camera_2_library, matches_library[i,0:2], matches_library[i,2:4]).reshape(1,4)
+        points_3d_library[i] = points_3d_library[i]/points_3d_library[i][-1]
+#        print(point)
+    camera_center1 = compute_camera_center(camera_1_library)
+    camera_center1 = camera_center1/camera_center1[-1]
+    
+    camera_center2 = compute_camera_center(camera_2_library)
+    camera_center2 = camera_center2/camera_center2[-1]
+    
+    fig = plt.figure()
+    fig.suptitle('3D reconstruction of the Library using OpenCV', fontsize=16)
+    ax = fig.add_subplot(111, projection="3d")
+    ax.scatter(camera_center1[0],camera_center1[1],camera_center1[2],c='r',marker='+',label="Camera 1 center")
+    ax.scatter(camera_center2[0],camera_center2[1],camera_center2[2],c='g',marker='+',label="Camera 2 center")
+    for i in range(len(points_3d_library)):
+        ax.scatter(points_3d_library[i][0],points_3d_library[i][1],points_3d_library[i][2],c='b')
+    plt.legend()
+    plt.axis('equal')
+    plt.xlabel("x axis")
+    plt.ylabel("y axis")
+#    plt.zlabel("z axis")
+    return points_3d_library
+    
+#open_cv_method = test_library_triangulation(matches_library,camera_1_library,camera_2_library)
+our_method = triangulation_plotting_library(matches_library,camera_1_library,camera_2_library)
+
+def test_house_triangulation(matches_house,camera_1_house,camera_2_house):
+    points_3d_house = np.zeros((len(matches_house),4),dtype=float)
+    for i in range(len(matches_house)):
+#        print(matches_house[i,2:4].shape)
+        points_3d_house[i] = cv2.triangulatePoints(camera_1_house, camera_2_house, matches_house[i,0:2], matches_house[i,2:4]).reshape(1,4)
+    camera_center1 = compute_camera_center(camera_1_house)
+    camera_center1 = camera_center1/camera_center1[-1]
+    
+    camera_center2 = compute_camera_center(camera_2_house)
+    camera_center2 = camera_center2/camera_center2[-1]
+    
+    fig = plt.figure()
+    fig.suptitle('3D reconstruction of the house using OpenCV', fontsize=16)
+    ax = fig.add_subplot(111, projection="3d")
+#    ax.scatter(camera_center1[0],camera_center1[1],camera_center1[2],c='r',marker='+',label="Camera 1 center")
+#    ax.scatter(camera_center2[0],camera_center2[1],camera_center2[2],c='g',marker='+',label="Camera 2 center")
+    for i in range(len(points_3d_house)):
+        ax.scatter(points_3d_house[i][0],points_3d_house[i][1],points_3d_house[i][2],c='b')
+    plt.legend()
+    plt.axis('equal')
+    plt.xlabel("x axis")
+    plt.ylabel("y axis")
+#    plt.zlabel("z axis")
+    return points_3d_house
+
+#opencv_house = test_house_triangulation(matches_house,camera_1_house,camera_2_house)
+#our_house = triangulation_plotting_house(matches_house,camera_1_house,camera_2_house)
+
+
+
     
     
     
